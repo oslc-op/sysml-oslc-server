b9e68fb1081b68b85a62f4e2ba5e14b4c8a0eb71
Andrew Berezovskyi <andriib@kth.se>
2022-02-08 14:09:31 +0100

    Use a ViewModel approach in the project commits
    Signed-off-by: Andrew Berezovskyi <andriib@kth.se>

--- org.oasis.oslcop.sysml.oslc-server/src/main/java/org/oasis/oslcop/sysml/services/StoreService.java
+++ org.oasis.oslcop.sysml.oslc-server/src/main/java/org/oasis/oslcop/sysml/services/StoreService.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -24,9 +24,12 @@ import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.eclipse.lyo.oslc4j.core.OSLC4JUtils;
 import org.eclipse.lyo.oslc4j.core.model.ServiceProvider;
+import org.eclipse.lyo.store.ModelUnmarshallingException;
 import org.eclipse.lyo.store.Store;
+import org.eclipse.lyo.store.StoreAccessException;
 import org.oasis.oslcop.sysml.SysmlServerManager;
 import org.oasis.oslcop.sysml.json.ProjectCommit;
+import org.oasis.oslcop.sysml.resources.view.ProjectCommitViewModel;
 import org.oasis.oslcop.sysml.servlet.ServiceProviderCatalogSingleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -81,9 +84,39 @@ public class StoreService
 
     @GET
     @Path("projectCommits")
-    public void getProjectCommits() throws IOException, ServletException {
-        httpServletRequest.setAttribute("projectCommits", projectCommits());
+    public void getProjectCommits() throws IOException, ServletException, StoreAccessException,
+            ModelUnmarshallingException {
+        List<String> commits = projectCommits();
+//        httpServletRequest.setAttribute("projectCommits", commits);
+//        Store store = SysmlServerManager.getStorePool().getStore();
+//        String id = projectCommits().get(0);
+//        try {
+//            List<ServiceProvider> serviceProviders =  store.getResources(StoreService.constructNamedGraphUri(id), ServiceProvider.class); //new ArrayList<ServiceProvider>();
+//        } catch (StoreAccessException e) {
+//            e.printStackTrace();
+//        } catch (ModelUnmarshallingException e) {
+//            e.printStackTrace();
+//        }
+
+
+        Store store = SysmlServerManager.getStorePool().getStore();
+        List <ProjectCommitViewModel> commitModels = new ArrayList<>();
+        for (String id : commits) {
+            List<ProjectCommitViewModel.ServiceProviderViewModel> providers =
+                    store.getResources(StoreService.constructNamedGraphUri(id),
+                                    ServiceProvider.class)
+                    .stream()
+                    .map(serviceProvider -> new ProjectCommitViewModel.ServiceProviderViewModel(serviceProvider.getIdentifier()))
+                    .collect(Collectors.toList());
+            URI uri = UriBuilder.fromUri(OSLC4JUtils.getServletURI())
+                    .path("/store/projectCommits/" + id).build();
+            commitModels.add(new ProjectCommitViewModel(id, id.equals(selectedProjectCommit), providers, uri));
+        }
+        SysmlServerManager.getStorePool().releaseStore(store);
+
+        httpServletRequest.setAttribute("projectCommits", commitModels);
         httpServletRequest.setAttribute("selectedProjectCommit", selectedProjectCommit);
+
         RequestDispatcher rd = httpServletRequest.getRequestDispatcher("/org/oasis/oslcop/projectCommits.jsp");
         rd.forward(httpServletRequest,httpServletResponse);
         return;
